75dddbe91c415a0e60beb136258e0b813d5e2a78
jabbourdan <111487226+jabbourdan@users.noreply.github.com>
2022-11-04 15:12:30 +0200

    security group for jenkins

---
+++ Terraform/securityG_for_jenkins.tf
@@ -0,0 +1,40 @@
+// Security to allow ssh on port 22, http port 80 ,https 443:
+resource "aws_security_group" "sg_jenkins" {
+  name        = "jenkins_sg"
+  description = "allow ssh,http,and https connection"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description = "TLS from internet"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow http internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow-web"
+  }
+}
